ac4e3380c6eb7315352c44cf8a92ede4c19891b7
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2022-10-25 12:31:44 -0400

    the forgotten home page

--- src/components/shared/InfoModal.svelte
+++ src/components/shared/InfoModal.svelte
@@ -2,7 +2,7 @@
     export let close;
 </script>
 
-<div class="modal">
+<div class="modal absolute w-2/4 h-2/4 m-auto items-center bg-slate-400">
     <div class="closeModal">
         <i class="fa fa-times" on:click={close}></i>
     </div>
